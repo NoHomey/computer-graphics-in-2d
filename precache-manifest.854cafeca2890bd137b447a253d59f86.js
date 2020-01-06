5076fb24e1f32d8165caf06972989a7e72ae9c2d
NoHomey <ivo.stratev.tues@gmail.com>
2020-01-06 13:42:01 +0200

    Updates

---
+++ precache-manifest.854cafeca2890bd137b447a253d59f86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82bf7818610b973f3b9f5cc33f6f28b",
+    "url": "/computer-graphics-in-2d/index.html"
+  },
+  {
+    "revision": "02a5da365c6b49db2f04",
+    "url": "/computer-graphics-in-2d/static/js/2.e2598d2c.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/computer-graphics-in-2d/static/js/2.e2598d2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a181c651cba5e10148d",
+    "url": "/computer-graphics-in-2d/static/js/main.207b541b.chunk.js"
+  },
+  {
+    "revision": "86a2e0c22b79dda23459",
+    "url": "/computer-graphics-in-2d/static/js/runtime-main.65161ca9.js"
+  }
+]);
